daa9643667ac579283bc9708046ce33ddee96c15
Rômulo Júnio <86726833+Romz16@users.noreply.github.com>
2022-03-29 21:04:15 -0300

    Feat:add Jogo/puzzle

--- SUDO.c
+++ SUDO.c
@@ -1,9 +1,9 @@
 #include <stdio.h>         //entrada/saída
 #include <math.h>          //fornece um conjunto de funções para operações matemáticas
 #include <stdlib.h>        //funções envolvendo alocação de memória, controle de processos, conversões e outras
-#include <string.h>        //fornece meios para a manipulação das strings
+#include <string.h>        //fornece meios para a manipulação das strings7
+#include <stdbool.h>
 #define TAM 9
-
 
 
 //Função para criar e alocar matriz de inteiros
@@ -33,6 +33,8 @@ void desalocarInt(int **matriz, int n)
     free(matriz);
 }
 
+
+
 //Função que irá imprimir o menu com os comandos possiveis
 char *MenuInicial()
 {
@@ -61,24 +63,205 @@ void Instrucoes()
    
 
     printf("Para jogar\n\n");
-    printf("Comandos:\n\n- \n\n");
-    printf("- Resolver o jogo todo -> resolver\n");
+    printf("-Comandos: Quando lhe for requerido as coordenadas passe primeiro X e apos Y, de forma que"); 
+    printf(" haja um espaço entre cada numero EX:[4 1],posteriormente informe algum numero de 1 a 9 \n\n");
+    printf("- Resolver o jogo todo -> passe uma das coordenadas como 0 ( 0, 1 )\n");
 }
 
-void TabuleiroInicializa(char *nomeArq){
-    int tabela[TAM][TAM];
+int** TabuleiroInicializa(char *nomeArq){
+    int i, j;
+    int** puzzle;
+    int arrayPuzzle[9][9];
+
     FILE *arq = fopen(nomeArq,"r");
-    for (int i = 0; i < TAM; i++)
+    for (int i = 0; i < 9; i++)
     {
-        for (int j = 0; j < TAM; j++)
+        for (int j = 0; j < 9; j++)
         
-            fscanf(arq,"%d",&tabela[i][j]);
+            fscanf(arq,"%d",&arrayPuzzle[i][j]);
         
     }
     fclose(arq);
-    printf("tudo ok ate aqui");
+
+    puzzle = (int**)malloc(sizeof(int*) * 9);
+
+    for (i = 0; i < 9; i++){
+        puzzle[i] = (int*)malloc(sizeof(int) * 9);
+        for(j = 0; j < 9; j++){
+            puzzle[i][j] = arrayPuzzle[i][j];
+        }
+    }
+    
+    return puzzle;
 }
 
+int** copiaPuzzle(int** puzzle){
+    int i, j;
+    int** copiaTab;
+
+    copiaTab = (int**)malloc(sizeof(int*) * 9);
+    for (i = 0; i < 9; i++){
+        copiaTab[i] = (int*)malloc(sizeof(int) * 9);
+        for(j = 0; j < 9; j++){
+            copiaTab[i][j] = puzzle[i][j];
+        }
+    }
+    return copiaTab;
+}
+
+bool valoresValidos(int** puzzle, int* linha, int* coluna){
+    int i, j;
+
+    for (i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            if (puzzle[i][j] == 0){
+                *linha = i;
+                *coluna  = j;
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+bool Ehvalido(int** puzzle, int linha, int coluna, int val){
+    int i, j;
+    int linhabloco, colunabloco;
+
+    //CHECK VERTICAL
+    for(i = 0; i < 9; i++){
+        if (puzzle[i][coluna] == val)
+            return false;
+    }
+
+    //CHECK HORIZONTAL
+    for(j = 0; j < 9; j++){
+        if (puzzle[linha][j] == val)
+            return false;
+    }
+
+    //CHECK SQUARE
+    linhabloco = linha - linha % 3;
+    colunabloco = coluna - coluna % 3;
+
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            if(puzzle[linhabloco + i][colunabloco + j] == val)
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool Resolver(int** puzzle){
+    int i, j, val;
+
+    if(!valoresValidos(puzzle, &i, &j))
+        return true;
+
+    for(val = 1; val < 10; val++){
+        if(Ehvalido(puzzle, i, j, val)){
+            puzzle[i][j] = val;
+            if(Resolver(puzzle))
+                return true;
+            else
+                puzzle[i][j] = 0;
+        }
+    }
+    return false;
+}
+
+
+void printPuzzle(int** puzzle){
+    int i, j, a;
+
+    printf("\n");
+    printf(" 0 | 1  2  3 | 4  5  6 | 7  8  9 | X\n");
+    printf(" ---------------------------------\n");
+    for (i = 0, a = 1; i < 9; i++, a++){
+        for(j = 0; j < 9; j++){
+            if (j == 0)
+                printf(" %d |", a);
+            else if ((j) % 3 == 0)
+                printf("|");
+            printf(" %d ", puzzle[i][j]);
+            if (j == 8)
+                printf("|");
+        }
+        printf("\n");
+        if ((i + 1) % 3 == 0)
+            printf(" ---------------------------------\n");
+    }
+    printf(" Y\n");
+}
+
+void Jogo(int** userPuzzle, int** tempPuzzle){
+    int i, j;
+    int posX, posY, userVal;
+
+    while(1){
+    if(!valoresValidos(userPuzzle, &i, &j)){
+        printf("\nPARABENS, VOCE CONSEGUIU RESOLVER SOZINHO!!!\n");
+        return;
+    }
+
+    
+
+    printf("\n ADICIONE OS VALORES PARA AS COORDENADAS NO FORMATO:\"X Y\":\n");
+    scanf("%d %d",&posX, &posY);
+    while(1){
+        if ((posX > 9) || (posY > 9) || (userPuzzle[posY - 1][posX - 1] != 0)){
+            printf("\nSinto muito, esse valor nao eh valido, tente novamente\n");
+            scanf("%d %d",&posX, &posY);
+        }
+        else if ((posX==0) || (posY==0))
+        {   getchar();
+            Resolver(userPuzzle);
+            printf("\n PUZZLE RESOLVIDO:\n");
+            printPuzzle(userPuzzle);
+            return;
+        }
+        
+        else {
+            posX -= 1;
+            posY -= 1;
+            break;
+        }
+
+    }
+
+
+    printf("\nAdicione um valor de  1 a 9\n");
+    scanf("%d", &userVal);
+    while(1){
+        if((userVal > 9) || (userVal < 1)){
+            printf("\nvalor invalido, tente novamente :\n");
+            scanf("%d", &userVal);
+        }
+        else break;
+    }
+
+    if(Ehvalido(userPuzzle, posY, posX, userVal))
+        userPuzzle[posY][posX] = userVal;
+    else{
+         userPuzzle[posY][posX] = userVal;
+         printf("Alguma coisa deu errado...  Invalidos:\n\n");
+    }
+    for (i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            tempPuzzle[i][j] = userPuzzle[i][j];
+        }
+    }
+
+  
+    getchar();
+    printPuzzle(userPuzzle);
+    }
+
+    return;
+}
 //Comeca um jogo novo;
 void NovoJogo()
 {
@@ -97,8 +280,14 @@ void NovoJogo()
         printf("O arquivo precisa ser .txt\n");
         exit(0);
     }
-    TabuleiroInicializa( nomeArq);
- 
+    int** puzzle = TabuleiroInicializa( nomeArq);
+    int** userPuzzle = copiaPuzzle(puzzle);
+    int** tempPuzzle = copiaPuzzle(puzzle);
+    printPuzzle(userPuzzle);
+    Jogo(userPuzzle,tempPuzzle);
+    free(puzzle);
+    free(userPuzzle);
+    free(tempPuzzle);
 }
 
 int main()
